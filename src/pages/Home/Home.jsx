80851591f0b5bc42d3446d3582fe79806f547bb1
Nazmul5691 <nazmulislam5691@gmail.com>
2023-10-13 11:16:01 +0600

    first commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import { useLoaderData } from "react-router-dom";
 import Categoris from "../../components/Categories/Categoris";
 import Banner from "../../components/Header/Banner/Banner";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 const Home = () => {
 
@@ -9,12 +11,15 @@ const Home = () => {
     const categoris = useLoaderData()
 
     // console.log(categoris);
-
+    
 
     return (
         <div>
             <Banner></Banner>
+            <div data-aos="fade-in"></div>
+            <div data-aos="fade-up" data-aos-anchor=".other-element"></div>
             <Categoris categoris={categoris}></Categoris>
+
 
         </div>
     );
